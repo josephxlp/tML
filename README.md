c022c79bdc578885afad8b0e81e5636438da05fa
jzphlp <josephluntadilap@gmail.com>
2025-05-04 01:54:35 +0100

    pagluon example how-to-use

---
+++ README.md
@@ -0,0 +1,4 @@
+
+- add all the types of problems  for amljar and agluon with the respective predictions 
+- add multi-lable agloun 
+- add fair with amljar 
--- tML/__init__.py
+++ tML/__init__.py
@@ -3,3 +3,11 @@
 __author__ = """J"""
 __email__ = "l.joseph.p@hotmail.com"
 __version__ = "0.0.1"
+
+from .pagluon import agluon_pipeline
+from .pamljar import amljar_pipeline
+
+__all__ = [
+    "agluon_pipeline",
+    "amljar_pipeline"
+]
